51a421dfe500bcc3b2b8e1f8798664ed41d8d229
Sanskar-sd <darekarsanskar@gmail.com>
2026-04-04 14:24:25 +0530

    finally apis are running and all good so far

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,8 @@ dotenv.config();
 connectDB();
 
 //start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
